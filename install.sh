c5a51a937e94250c32e1d9b6a42e66bc62ac9e13
David Denton <denton.david@gmail.com>
2018-03-16 18:25:56 +0000

    added chart museum

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 helm init
+helm repo add incubator https://kubernetes-charts-incubator.storage.googleapis.com
 helm dependency update
 helm delete k8s-env
 helm upgrade --install k8s-env .
