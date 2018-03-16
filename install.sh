9f45b18c8129d8d532d332a4158ad68f1c64c5e0
David Denton <denton.david@gmail.com>
2018-03-16 18:50:13 +0000

    added heapster

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 helm init
 helm repo add incubator https://kubernetes-charts-incubator.storage.googleapis.com
 helm dependency update
-helm delete k8s-env
+helm delete --purge k8s-env
 helm upgrade --install k8s-env .
